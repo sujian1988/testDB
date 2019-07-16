520aa7a5a1fdad4fb4a752cc5e42fbe61c0149e7
sujian1988 <597201889@qq.com>
2019-07-16 12:40:54 +0900

    day01完成评论功能

--- www/day01/message/app.js
+++ www/day01/message/app.js
@@ -4,6 +4,12 @@ var url = require('url')
 var http = require('http')
 var querystring = require('querystring')
 
+//创建留言数据对象
+var msgs = [
+	{name: '张三', content: "你好我是张三", create_at: '2017-11-14 10:30:32'},
+	{name: '李四', content: "你好我是李四", create_at: '2017-11-15 10:11:14'},
+	{name: '王五', content: "你好我是王五", create_at: '2017-11-16 10:22:55'}
+  ];
 
 //2 创建服务器
 var server = http.createServer();
@@ -17,6 +23,22 @@ server.on('request', function(req, res){
 	//请求“/”加载留言板列表
 		fs.readFile('./view/index.html', 'utf8', function(err, data){
 			if (err) res.end('404 Not Found');
+
+			var html = '';
+			//循环输出输出显示变量
+			msgs.forEach(function(item){
+				//<li class="list-group-item">范家伟说：香锅还是香锅啊 <span class="pull-right">2017-11-2 17:11:22</span></li>
+				//传统：通过 + 拼接  遇到遍历 ++
+				//ES6语法：反引号，遍历${变量名}输出（JS最新语法）
+				html += `
+					<li class="list-group-item">
+						${item.name}说：${item.content}
+						<span class="pull-right">${item.create_at}</span>
+					</li>
+				`
+			})
+			var data = data.replace('^_^', html);
+
 			res.setHeader('Content-Type', 'text/html;charset=utf-8');
 			res.write(data)
 			res.end()
@@ -30,7 +52,7 @@ server.on('request', function(req, res){
 			res.write(data)
 			res.end()
 		})
-	}  else if (currentUrl.indexOf('/public') === 0) {
+	}  else if (currentUrl.indexOf('/public') === 0) { //静态资源会单独发出请求bootstrap.css
 	//检测静态资源并响应（略难）
         console.log('检测静态资源并响应（略难）');
         fs.readFile('./' + currentUrl, 'utf8', function(err, data){
@@ -38,18 +60,20 @@ server.on('request', function(req, res){
 		   // res.setHeader('Content-Type', 'text/html;charset=utf-8');
 			res.write(data)
 			res.end()
-        })
-    } else if (currentUrl.indexOf('/doadd') == 0) {
+		})
+	} else if (currentUrl.indexOf('/doadd') == 0) {//url路径包含doadd就为真
         
         //表单提交数据处理
-        if (req.method == 'POST') {
+        if (req.method == 'POST') {  //post传大数据 get传小数据
             //明确：表单post提交的数据可能会非常大，所以分片获取
             //说明：data事件-数据传输中，end事件-数据传输完毕
             var postData = '';
             req.on('data', function (chunk) {
-                postData += chunk; 
+				postData += chunk; 
+				console.log('数据接收中...');
             }); 
             req.on('end', function () {
+				console.log('数据接收完毕.');
                 //通过querystring模块将字符串数据转化为对象
                 paramObj = querystring.parse(postData); 
                 console.log(paramObj)
@@ -64,8 +88,8 @@ server.on('request', function(req, res){
                 res.setHeader('Location', '/')
                 res.end()
             });
-        }else {
-			//1.接受数据
+        }else { // get请求方法
+			//1.通过url来获取数据
 			var paramObj = url.parse(req.url, true).query
 			console.log(paramObj)
 			//2.入库（压入到数组 push pop）
@@ -73,6 +97,7 @@ server.on('request', function(req, res){
 			var date = d.getFullYear()+'-'+d.getMonth()+'-'+d.getDate()+' '+
 			         d.getHours()+'-'+d.getMinutes()+'-'+d.getSeconds();
 			var msg = {name: paramObj.name, content: paramObj.content, create_at: date}
+			//push 将数据压入数组
 			msgs.push(msg)
 			//3.跳转
 			res.statusCode = 302;
